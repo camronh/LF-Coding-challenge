e31787bfe230b2c473922e48f8c01127f00a0787
camronh <camronhaider@gmail.com>
2022-10-17 15:20:38 -0400

    Added Swagger UI gen

--- routes/submissions.js
+++ routes/submissions.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /supervisors
+ * @returns {object} 200 - An array of user info
+ * @returns {Error}  default - ERROR: Failed to fetch supervisors
+ */
 router.get("/supervisors", async (req, res) => {
   try {
     console.log("Fetching and sorting managers...");
@@ -26,10 +32,27 @@ router.get("/supervisors", async (req, res) => {
   }
 });
 
+/**
+ * @typedef Submission
+ * @property {string} firstName.required - First name of the user
+ * @property {string} lastName.required - Last name of the user
+ * @property {string} supervisor.required - Supervisor of the user
+ * @property {string} email - Email of the user
+ * @property {string} phoneNumber - Phone number of the user
+ */
+
+/**
+ *
+ * This function comment is parsed by doctrine
+ * @route POST /submit
+ * @param {Submission.model} submission.body.required - Submission data
+ * @returns {string} 201 - SUCCESS: Form submitted
+ * @returns {Error}  default - ERROR: Missing required fields
+ */
 router.post("/submit", async (req, res) => {
   try {
-    console.log("REQUEST BODY:\n", req.body);
     const { firstName, lastName, email, phoneNumber, supervisor } = req.body;
+    console.log("POST BODY:\n")
     console.log({ firstName, lastName, email, phoneNumber, supervisor });
     if (!firstName || !lastName || !supervisor) throw "Missing required fields";
     res.status(201).send("SUCCESS: Form submitted");
--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 // For starting the express server locally without effecting testing
 const app = require("./index");
+const expressSwagger = require("express-swagger-generator")(app);
+
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: "This is a sample server",
+      title: "Swagger",
+      version: "1.0.0",
+    },
+    host: "localhost:9000",
+    basePath: "/api",
+    produces: ["application/json"],
+    schemes: ["http"],
+  },
+  basedir: __dirname, //app absolute path
+  files: ["./routes/**/*.js"], //Path to the API handle folder
+};
+expressSwagger(options);
 
 const port = 9000;
 
 app.listen(port, () => {
   console.log("Server has started! at http://localhost:9000");
+  console.log("Swagger GUI available at http://localhost:9000/api-docs");
+  console.log("API Logs will be printed here:\n")
 });
